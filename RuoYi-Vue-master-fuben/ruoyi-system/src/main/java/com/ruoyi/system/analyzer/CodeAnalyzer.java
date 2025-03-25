6faa6e0989ead62e9873cccf89c79b706bd89909
ruciyingjun <19949317733@163.com>
2025-03-25 21:04:58 +0800

    second commit

--- RuoYi-Vue-master-fuben/ruoyi-system/src/main/java/com/ruoyi/system/analyzer/CodeAnalyzer.java
+++
@@ -1,7 +0,0 @@
-package com.ruoyi.system.analyzer;
-
-import com.ruoyi.system.domain.CodeAnalysisReport;
-
-public interface CodeAnalyzer {
-    CodeAnalysisReport analyze(String code);
-} 
--- RuoYi-Vue-master-fuben/ruoyi-ui/src/constants/ai.js
+++
@@ -1,4 +0,0 @@
-export const AI_CONFIG = {
-  MODEL_NAME: '通义千问',  // 与后端 AiConfig.MODEL_NAME 保持一致
-  MODEL_TYPE: 'qwen-turbo'  // 与后端 AiConfig.MODEL_TYPE 保持一致
-}
